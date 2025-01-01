0609a12750f230253b58d9a3f511d8700384d05b
jiwonLee42 <ljw56b@gmail.com>
2025-01-01 16:17:22 +0900

    FEAT: apiresponse 추가

---
+++ src/main/java/com/app/treen/common/response/code/status/SuccessStatus.java
@@ -0,0 +1,45 @@
+package app.jasople.Config.code.status;
+
+import app.jasople.Config.code.BaseCode;
+import app.jasople.Config.code.ReasonDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum SuccessStatus implements BaseCode {
+
+    // 일반적인 응답
+    _OK(HttpStatus.OK, "COMMON200", "성공입니다."),
+
+    // 멤버 관련 응답
+
+    // ~~~ 관련 응답
+    PIN_LIKE(HttpStatus.OK, "LIKE200", "좋아요 등록에 성공하였습니다."),
+    PIN_UNLIKE(HttpStatus.OK, "LIKE201", "좋아요 취소를 성공하였습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    @Override
+    public ReasonDTO getReason() {
+        return ReasonDTO.builder()
+                .message(message)
+                .code(code)
+                .isSuccess(true)
+                .build();
+    }
+
+    @Override
+    public ReasonDTO getReasonHttpStatus() {
+        return ReasonDTO.builder()
+                .message(message)
+                .code(code)
+                .isSuccess(true)
+                .httpStatus(httpStatus)
+                .build()
+                ;
+    }
+}
