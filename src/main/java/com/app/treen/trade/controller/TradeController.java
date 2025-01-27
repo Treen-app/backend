b91d454e0a0e965950ec5a1f2906060015a69801
Leeyoujin00 <cymn6032@gmail.com>
2025-01-27 19:50:32 +0900

    [FEAT] 자유교환 생성

---
+++ src/main/java/com/app/treen/trade/controller/TradeController.java
@@ -0,0 +1,13 @@
+package com.app.treen.trade.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/trade")
+public class TradeController {
+
+
+}
