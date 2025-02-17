3ac18f02f72975448cf151808b4b6f63283eeea3
Leeyoujin00 <cymn6032@gmail.com>
2025-02-17 18:36:59 +0900

    [TEST] DataSource 연결 비활성화

--- src/test/java/com/app/treen/TreenApplicationTests.java
+++ src/test/java/com/app/treen/TreenApplicationTests.java
@@ -2,13 +2,14 @@ package com.app.treen;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@EnableAutoConfiguration(exclude = {RedisAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {RedisAutoConfiguration.class, DataSourceAutoConfiguration.class})
 class TreenApplicationTests {
 
 	@Test
