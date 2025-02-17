a6edae88fbceb3f5c598468f322f148943e62598
Leeyoujin00 <cymn6032@gmail.com>
2025-02-17 18:29:33 +0900

    [TEST] redis 연결  비활성화

--- src/test/java/com/app/treen/TreenApplicationTests.java
+++ src/test/java/com/app/treen/TreenApplicationTests.java
@@ -1,9 +1,14 @@
 package com.app.treen;
 
-import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@EnableAutoConfiguration(exclude = {RedisAutoConfiguration.class})
 class TreenApplicationTests {
 
 	@Test
